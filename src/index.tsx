6f8ff2ffd1e77f62381a4bfbe79cc42e8bd83207
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-04-04 18:06:10 +0200

    fix: removing devtools support from the hook

--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,16 @@
 import * as React from 'react';
-import {Store, DevToolsInstance, getExtension, isRelevant} from "react-rxjs";
-import {tap} from "rxjs/operators";
+import { Store } from "react-rxjs";
+import { tap } from "rxjs/operators";
 
 export function useStore<StoreProps>(store: Store<StoreProps, StoreProps>,
                                      initialProps: StoreProps) {
   const [state, setState] = React.useState<StoreProps>(initialProps);
   React.useEffect(() => {
-    let devToolsInstance: DevToolsInstance
-    let devToolsSubscription: () => void;
-    let devToolsExtension = getExtension()
-    if (devToolsExtension) {
-      devToolsInstance = devToolsExtension.connect()
-      devToolsSubscription = devToolsInstance.subscribe(message => {
-        if (isRelevant(message)) {
-          setState(JSON.parse(message.state))
-        }
-      })
-    }
-    const observable = typeof store !== "function" ? store : store(initialProps);
+    const observable = typeof store !== "function" ? store : store(state);
     const subscription = observable
-      .pipe(tap(setState), tap(state => {
-        if (devToolsInstance) {
-          devToolsInstance.send("update", state);
-        }
-      }))
+      .pipe(tap(setState))
       .subscribe()
-    return () => {
-      if (devToolsSubscription) {
-        devToolsSubscription()
-      }
-      subscription.unsubscribe();
-    };
+    return () => subscription.unsubscribe();
   }, []);
   return state;
 }
